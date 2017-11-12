89cee725842f82c391b9fab42f46c90cb1c21385
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-12 21:52:28 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -16,5 +16,5 @@ class Question {
 }
 Question._All = [];
 
-let newQuestion = new Question("Where in the world is carmen sandiego?")
+let newQuestion = new Question("What's your favorite food?")
 newQuestion.save()
