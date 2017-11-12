969db29bf1fb0d6097e9228cd85f5da449feeeda
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-12 21:43:29 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,20 @@
 'use strict';
+class Question {
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  static All(){
+    return this._All;
+  }
+  static Find(id){
+    return this.All()[id - 1]
+  }
+}
+Question._All = []
+
+let newQuestion = new Question("Where in the world is carmen sandiego?")
+newQuestion.save()
